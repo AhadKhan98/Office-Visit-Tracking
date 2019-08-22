da67552df02f5c011a8f5b9bcf984c889ea76acc
AhadKhan98 <zaia@berea.edu>
2019-08-22 13:16:14 -0400

    created main function & added imports

---
+++ script/main.py
@@ -0,0 +1,22 @@
+"""
+
+This program is built for the Labor Office at Berea College. The purpose of this program
+is to utilize a barcode scanner to scan for student id numbers and find information such as
+full name, date of birth, reason for visit. The information is then saved into a spreadsheet
+in the labor program's smartsheet account.
+@author: Ahad Zai
+
+"""
+
+import os
+from simple_smartsheet import Smartsheet
+from simple_smartsheet.models import Sheet, Column, Row, Cell, ColumnType
+
+def setup_smartsheets():
+    return "Hello"
+
+def main():
+    print("Hello")
+
+if __name__=="__main__":
+    main()
